158b6c28fe96ff8e252fa45e1eddad38e23e47f7
Diego-lopez-chuello <dcontador14@gmail.com>
2023-09-25 03:56:01 +0000

    601_ec_01_20

--- src/workflow-inicial/621_DR_corregir_drifting_ec_01_20p.r
+++ src/workflow-inicial/621_DR_corregir_drifting_ec_01_20p.r
@@ -11,9 +11,9 @@ require("yaml")
 
 # Parametros del script
 PARAM <- list()
-PARAM$experimento <- "DR6210_EC_03"
+PARAM$experimento <- "DR6210_EC_01_20"
 
-PARAM$exp_input <- "CA6110_EC_01"
+PARAM$exp_input <- "CA6110_EC_01_20"
 
 PARAM$variables_intrames <- TRUE # atencion esto esta en TRUE
 
